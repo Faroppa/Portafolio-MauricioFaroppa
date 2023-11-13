73f8e4bfdbf272eae0ddf8bf6d830df1d3a09fb5
Faroppa <150689750+Faroppa@users.noreply.github.com>
2023-11-13 22:16:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function abrirMenu() {
+  const menu = document.querySelector(".links-menu");
+  const icono = document.querySelector(".icono-hamburguesa");
+  menu.classList.toggle("open");
+  icono.classList.toggle("open");
+}
